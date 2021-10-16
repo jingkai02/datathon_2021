31939f0256b0dcb0ecfc1773dca88b774a665b64
Jing Kai <poohjk02@gmail.com>
2021-10-16 13:57:49 -0500

    updared early tables

--- major.py
+++ major.py
@@ -1,11 +1,29 @@
 class Major:
-    def __init__(self, name):
+    def __init__(self, name, cat, url):
         self.name = name
+        self.cat = cat
+        self.url = url
+    
+    def categories(self):
+        self.classes = Classes()
+        
     
     
-class Classes:
-    def __init__(self, credits, ):
+class Category():
+    def __init__(self, name, credits, ):
         self.credits = credits
+    def classes():
+        self.classes = 
+        
+        
+            
+class Classes():
+    def __init__(self, class_no):
+        self.class_no = class_no
+            
+    
+class Student():
+    def __init__(self):
         pass
     
     
